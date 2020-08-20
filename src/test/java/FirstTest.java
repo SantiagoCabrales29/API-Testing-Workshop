364d2c9254ad3939bd17f40c1b822f16fc7c4420
scabrales <scabrales@mcclatchyservices.com>
2020-08-20 18:09:17 -0500

    Basic Tests

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -2,6 +2,7 @@ import io.restassured.http.ContentType;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.greaterThan;
 
 public class FirstTest {
 
@@ -12,9 +13,48 @@ public class FirstTest {
 		when()
 				.get("https://official-joke-api.appspot.com/random_joke").
 		then().
+
+				assertThat().
+				statusCode(200);
+	}
+
+
+	@Test
+	public void secondTest(){
+		given().
+				contentType(ContentType.JSON).
+		when()
+				.get("https://official-joke-api.appspot.com/random_joke").
+		then().
 				log().
 				all().
 				assertThat().
+				statusCode(200);
+	}
+
+	@Test
+	public void thirdTest(){
+		given().
+				contentType(ContentType.JSON).log().uri().
+		when()
+				.get("https://official-joke-api.appspot.com/random_joke").
+		then().
+				log().
+				body().
+				assertThat().
+				body("id", greaterThan(0)).
+				statusCode(200);
+	}
+
+	@Test
+	public void restfulBookerFirstTest(){
+		given().
+				contentType(ContentType.JSON).
+		when()
+				.get("https://restful-booker.herokuapp.com/booking").
+		then().
+				assertThat().
+				log().all().
 				statusCode(200);
 	}
 }
