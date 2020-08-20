71d74491039604c583a1a09b83da626b6961e417
scabrales <scabrales@mcclatchyservices.com>
2020-08-20 18:41:57 -0500

    RequestAndResponseSpecification

--- src/test/java/restfulbookerTests/restfulbookerBasicTests.java
+++ src/test/java/restfulbookerTests/restfulbookerBasicTests.java
@@ -1,38 +1,57 @@
 package restfulbookerTests;
 
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
 public class restfulbookerBasicTests {
+	private static RequestSpecification requestSpecification;
+	private static ResponseSpecification responseSpecification;
+
+	@BeforeClass
+	public static void createRequestSpecification(){
+		requestSpecification = new RequestSpecBuilder().
+				setContentType(ContentType.JSON).
+				setBaseUri("https://restful-booker.herokuapp.com/booking").
+				build();
+	}
+
+	@BeforeClass
+	public static void createResponseSpecification(){
+		responseSpecification = new ResponseSpecBuilder().
+				expectContentType(ContentType.JSON).
+				expectStatusCode(200).
+				build();
+	}
 
 	@Test
 	public void restfulBookerFirstTest(){
 		given().
 				contentType(ContentType.JSON).
-				when()
-				.get("https://restful-booker.herokuapp.com/booking").
-				then().
-				assertThat().
-				log().all().
-				statusCode(200);
+				spec(requestSpecification).
+		when().
+				get("").
+		then().
+				spec(responseSpecification);
 	}
 
 	@Test
 	public void testBookerIdValue(){
 		given().
-				contentType(ContentType.JSON).
-				when()
-				.get("https://restful-booker.herokuapp.com/booking").
-				then().
+				spec(requestSpecification).
+		when()
+				.get("").
+		then().
+				spec(responseSpecification).
 				assertThat().body("bookingid[0]",equalTo(12));
 	}
-
-
-
-
 
 	@Test
 	public void BookingByIdEndpointFirstTest(){
